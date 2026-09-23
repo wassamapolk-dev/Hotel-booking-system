c5cf3c2e495d6e4be5a8a46feb35c5e5ca9da570
Aungpao-2 <kittiphon.y@ku.th>
2026-09-23 15:24:45 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,3 +1,21 @@
+import javax.swing.*;
+import javax.swing.SwingUtilities;
+
 public class main {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater
+        (
+                () -> 
+            {
+                JFrame window = new JFrame("KU.Hotel.com");
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                window.setSize(800, 500);
+                window.setLocationRelativeTo(null);
+
+                // สั่งเปิดหน้า Login
+                window.setContentPane(Login.createPanel(window));
+                window.setVisible(true);
+            }
+        );
+    }
 }
