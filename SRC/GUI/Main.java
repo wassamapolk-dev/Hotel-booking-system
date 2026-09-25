40d2fea46fe7e0f3250b26b4cadda5bb564d37bd
Aungpao-2 <kittiphon.y@ku.th>
2026-09-25 11:57:05 +0700

    Add files via upload

---
+++ SRC/GUI/Main.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+
+        Register register = new Register();
+        register.setVisible(true);
+    }
+}
